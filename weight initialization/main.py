6153563d864e1433255efd9394dca5d67cc3fbdf
bams <danielbamilosin@gmail.com>
2026-02-15 21:20:10 +0100

    done

--- weight initialization/main.py
+++ weight initialization/main.py
@@ -3,11 +3,12 @@ import torch.nn as nn
 import torch.nn.init as init
 
 # weight initialization helps models converge better and faster, It helps prevent vanishing an exploding gradients
+# it also helps to break symmetry, prevents all neurons from learning the same thing.
 
 # you use different initializations based on the model architecture
 # xavier (glorot) initialization -> for tanh and sigmoid activations
 # kaiming (he) initialization for relu and relu variants (leaky relu...)
-# constant intializatino is often used for biases to ensure a neurtal starting point
+# constant intialization is often used for biases to ensure a neurtal starting point
 
 # reproducibility
 torch.manual_seed(42)
@@ -24,7 +25,7 @@ def initialize_weights(m):
     if isinstance(m, nn.Linear):
         init.xavier_uniform_(m.weight)
         if m.bias is not None:
-            init.zeros_(m.bias)
+            init.zeros_(m.bias) # make bias 0, bias can be zero becasuse they act as an offset for the activatio function, they dont necessarily help 'break symmetry'
     elif isinstance(m, nn.Conv1d):
         init.kaiming_uniform_(m.weight, mode="fan_out", nonlinearity='relu')
         # fan_out -> preserves weight magnitudes in the backward pass
